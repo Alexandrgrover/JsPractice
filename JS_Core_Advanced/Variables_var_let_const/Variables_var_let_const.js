d9c6a8db755766824c9b32d0fc3605ed1855ae3a
Grover <sashabank03@gmail.com>
2025-02-21 21:11:10 +0300

    use_strict

---
+++ JS_Core_Advanced/Variables_var_let_const/Variables_var_let_const.js
@@ -0,0 +1,8 @@
+console.log(a); // undefined
+var a = 10;
+
+console.log(b); // ReferenceError: Cannot access 'y' before initialization
+let b = 20;
+
+console.log(c); // ReferenceError: Cannot access 'z' before initialization
+const c = 30;
